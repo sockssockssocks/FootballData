9df136dbbda0ad56d9e0569fbfcddda577bdb5ba
sockssockssocks <tomshort3@gmail.com>
2020-03-08 12:32:05 +0000

    Last commit wasn't working correctly due to the updating. I fixed this problem. Will revisit this area of the code in the future to condense, which I'm sure can be done.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import http.client
 import json
 import sqlite3
 import matplotlib.pyplot as plt
+from itertools import chain
 from sqlite3 import Error
 from datetime import date
 
@@ -104,63 +105,73 @@ def select_from_table(connection, select_command):
         data = c.fetchall()
 
         formatted_data = [item for t in data for item in t]
-        return formatted_data
+
+        if not formatted_data:
+            print("Nothing returned from query")
+            return False
+        else:
+            return formatted_data
     except Error as e:
         print(e)
 
 
-# Updates the top_scorer with the new total of goals.
-def update_top_scorer(connection, data, player_id, number_of_goals):
+# Updates the top_scorer with the new total of goals and inserts new scorers.
+def update_top_scorer(connection, data):
     try:
         c = connection.cursor()
+        player_names_for_rows_created = []
+        player_names_for_updates_made = []
 
-        table_rows = c.execute("SELECT * FROM top_scorer").rowcount
-        table_rows = c.fetchall()
+        # Loop for length of data retrieved because it will never be shorter than the length of table.
+        for i, value in enumerate(data):
+            data_i_row_player_id = data[i][0]
+            data_i_row_player_id_exists_check = "SELECT * FROM top_scorer WHERE player_id=" + str(data_i_row_player_id)
 
-        changes_not_needed = 0
-        changes_made = 0
+            data_i_row_select_all = select_from_table(connection, data_i_row_player_id_exists_check)
 
-        # Iterates through the table, using a SELECT command for each row
-        for i, value in enumerate(table_rows):
-            execute_command = "SELECT player_id, number_of_goals FROM top_scorer WHERE player_id=" + str(value[0]) + ";"
-            c.execute(execute_command)
-            row_player_id = [item for t in c.fetchall() for item in t]
+            # If select_from_table returns False then insert these players into the table
+            if not data_i_row_select_all:
+                insert_into_top_scorer(connection, data[i])
+                player_names_for_rows_created.append(data[i][1])
+            else:
+                data_i_row_goals_scored = data[i][4]
 
-            # Reformat this to be neater. I don't think I need to have the else if I work it differently
-            # Checks to make sure won't get index out of range error as ifs look at the data received
-            if i < len(data):
-                # If player_id in the table matches the player_id in the request
-                if row_player_id[0] == value[0] in player_id and row_player_id[0] == value[0] in data[i]:
-                    # If the number of goals scored in the table matches the request
-                    if row_player_id[1] == value[4] in number_of_goals and row_player_id[1] == value[4] in data[i]:
-                        changes_not_needed += 1
-                    else:
-                        execute_command = ("UPDATE top_scorer SET player_id=?, player_name=?, player_club_id=?,"
-                                            "player_club_name=?, number_of_goals=?, player_position=? WHERE player_id=" +
-                                            str(row_player_id[0]))
-                        execute_command = "".join(str(execute_command))
-                        c.execute(execute_command, data[i])
-                        connection.commit()
-                        changes_made += 1
+                if data_i_row_goals_scored != data_i_row_select_all[4]:
+                    execute_command = ("UPDATE top_scorer SET player_id=?, player_name=?, player_club_id=?,"
+                                       "player_club_name=?, number_of_goals=?, player_position=? WHERE player_id="
+                                       + str(data_i_row_player_id))
+                    execute_command = "".join(str(execute_command))
+                    c.execute(execute_command, data[i])
+                    connection.commit()
 
-        print("Number of changes made:", changes_made)
+                    player_names_for_updates_made.append(data[i][1])
+
+        if player_names_for_rows_created:
+            print("Created", len(player_names_for_rows_created), "new rows for the following player/s:")
+            print(*player_names_for_rows_created, sep="\n")
+        if player_names_for_updates_made:
+            print("Updated", len(player_names_for_updates_made), "rows for the following player/s:")
+            print(*player_names_for_updates_made, sep="\n")
+
     except Error as e:
         print(e)
 
 
-# Insert the scorers to the top_scorer database. If data exists it will not
+# Insert the scorers to the top_scorer database. If data exists it will not.
 def insert_into_top_scorer(connection, data):
     try:
         c = connection.cursor()
-        c.executemany("""INSERT INTO top_scorer VALUES (?,?,?,?,?,?);""", data)
+        c.execute("""INSERT INTO top_scorer VALUES (?,?,?,?,?,?);""", data)
         connection.commit()
     except Error as e:
         print(e)
 
 
-# Idea: Create enum for club ID to primary colour on badge to represent the bar colour
-def plot_top_five_scorers(data):
-    # Not best way to do this for sure
+# Creates a bar chart of the top five scorers from any data given.
+# Needs work
+def plot_top_five_scorers(data, graph_title):
+    print(data)
+
     players = [data[1], data[7], data[13], data[19], data[25]]
     goals = [data[4], data[10], data[16], data[22], data[28]]
     positions = [data[5], data[11], data[17], data[23], data[29]]
@@ -169,7 +180,7 @@ def plot_top_five_scorers(data):
     for index, names in enumerate(players):
         players[index] = names[names.index(" "):]
 
-    # What does underscore do?
+    # For loop without iterator which saves me from having to find the length of players
     x_pos = [i for i, _ in enumerate(players)]
 
     # Generates the colour of bars and the legend.
@@ -192,14 +203,17 @@ def plot_top_five_scorers(data):
     bars = plt.bar(x_pos, goals, align="center", alpha=0.3, color=colors)
 
     axes = plt.gca()
-    axes.set_ylim([0, goals[0] + 6])  # Increase this without .5 showing on the y axis
+    axes.set_ylim([0, goals[0] + 6])
 
     plt.ylabel("Goals Scored")
     plt.xlabel("Player Name")
-    plt.title("Top Five Goalscorers in Premier League")
+    plt.title("Top Five Goalscorers (" + graph_title + ") in Premier League")
     plt.legend(bars, legend)
 
     plt.xticks(x_pos, players)
+
+    filename = "C:/Users/tomto/PycharmProjects/Football/" + get_usable_date() + graph_title
+    plt.savefig(filename, bbox_inches='tight')
 
     plt.show()
 
@@ -239,20 +253,36 @@ def main():
     # If table is empty, fill with new call. If not empty then update any rows that need updating
     if not select_from_table(database_connection, sql_select_all):
         print("top_scorer is empty")
-        insert_into_top_scorer(database_connection, request_received)
+        insert_into_top_scorer(database_connection, request_received) # TEST THIS ACTUALLY WORKS
         print("Table created with ", len(request_received), " rows.")
     else:
         player_id = select_from_table(database_connection, sql_goals_scored_player_id)
         number_of_goals = select_from_table(database_connection, sql_goals_scored_number_of_goals)
 
         # Commits the received request to the database
-        update_top_scorer(database_connection, request_received, player_id, number_of_goals)
+        update_top_scorer(database_connection, request_received)
 
-    plot_top_five_scorers(select_from_table(database_connection, sql_select_all))
-    # plot_top_five_scorers(select_from_table(database_connection, "SELECT * FROM top_scorer WHERE player_position='Midfielder'"))
-    # plot_top_five_scorers(select_from_table(database_connection, "SELECT * FROM top_scorer WHERE player_position='Defender'"))
+    '''top_scoring_attacker = (select_from_table(database_connection,
+                            "SELECT player_name, MAX (number_of_goals), player_position FROM top_scorer WHERE player_position='Attacker'"))
+    top_scoring_midfielder = (select_from_table(database_connection,
+                              "SELECT player_name, MAX (number_of_goals), player_position FROM top_scorer WHERE player_position='Midfielder'"))
+    top_scoring_defender = (select_from_table(database_connection,
+                            "SELECT player_name, MAX (number_of_goals), player_position FROM top_scorer WHERE player_position='Defender'"))'''
 
-    # print(select_from_table(database_connection, "SELECT * FROM top_scorer WHERE player_position='Midfielder'"))
+    '''top_scoring_in_position = [top_scoring_attacker, top_scoring_midfielder, top_scoring_defender]
+    top_scoring_in_position = list(chain.from_iterable(top_scoring_in_position))'''
+
+    '''plot_top_five_scorers(select_from_table(database_connection, sql_select_all), "All Positions")
+    plot_top_five_scorers(select_from_table(database_connection,
+                                            "SELECT * FROM top_scorer WHERE player_position='Attacker'"),
+                          "Attackers")
+    plot_top_five_scorers(select_from_table(database_connection,
+                                            "SELECT * FROM top_scorer WHERE player_position='Midfielder'"),
+                          "Midfielders")
+    plot_top_five_scorers(select_from_table(database_connection,
+                                            "SELECT * FROM top_scorer WHERE player_position='Defender'"),
+                          "Defenders")
+    # plot_top_five_scorers(top_scoring_in_position, "In Each Position")'''
 
     close_connection(database_connection)
 
@@ -260,17 +290,17 @@ def main():
 if __name__ == "__main__":
     main()
 
-
 '''
 Start my own goalscorer tally:
-- create a call to the API that gets all matches played on current matchday and return playerIDs and numbers of goals scored.
-- will need to check if I need to write to the db in the first place.
 - add a table per game week so I can see averages etc.
+- call the API and work out number of goals scored per country
 
 Create a table for each player with over 3 goals to store when they were scored. Can make graphs from that.
 
+Could you connection.rollback() in excepts
 
 '''
+
 '''
 GRAVEYARD:
 
