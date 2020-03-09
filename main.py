2e250321f35ea882df5248b828f799f897caa93a
Tom Short <tomshort3@gmail.com>
2020-03-09 18:02:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import http.client
 import json
 import sqlite3
+import matplotlib.pyplot as plt
+import numpy as np
 from sqlite3 import Error
 from datetime import date
 
@@ -12,18 +14,17 @@ def make_request(api_token):
         print("Response received.")
         connection = http.client.HTTPConnection('api.football-data.org')
         headers = {'X-Auth-Token': api_token}
-        #connection.request('GET', '/v2/competitions/PL/', None, headers)
-        #response = json.loads(connection.getresponse().read().decode())
+        connection.request('GET', '/v2/competitions/PL/', None, headers)
+        response = json.loads(connection.getresponse().read().decode())
 
-        '''
-        league_id = response.get("id")
+        league_id = response.get("name")
         current_season = response.get("currentSeason").get("id")
         current_matchday = response.get("currentSeason").get("currentMatchday")
+        last_updated = response.get("lastUpdated")
         information = {league_id: league_id, current_season: current_season, current_matchday: current_matchday}
-        '''
 
-        # Limited at 200 but this could change.
-        connection.request('GET', '/v2/competitions/PL/scorers?limit=200', None, headers)
+        # Limited at 500 but this could change.
+        connection.request('GET', '/v2/competitions/PL/scorers?limit=500', None, headers)
         response = json.loads(connection.getresponse().read().decode())
 
         top_scorers = []
@@ -38,12 +39,14 @@ def make_request(api_token):
             team_id = response.get("scorers")[i].get("team").get("id")
             team_name = response.get("scorers")[i].get("team").get("name")
             goals_scored = response.get("scorers")[i].get("numberOfGoals")
+            player_position = response.get("scorers")[i].get("player").get("position")
 
             list_to_add.append(player_id)
             list_to_add.append(player_name)
             list_to_add.append(team_id)
             list_to_add.append(team_name)
             list_to_add.append(goals_scored)
+            list_to_add.append(player_position)
 
             top_scorers.append(list_to_add)
 
@@ -102,53 +105,120 @@ def select_from_table(connection, select_command):
         data = c.fetchall()
 
         formatted_data = [item for t in data for item in t]
-        return formatted_data
+
+        if not formatted_data:
+            print("Nothing returned from query")
+            return False
+        else:
+            return formatted_data
     except Error as e:
         print(e)
 
 
-# Updates the top_scorer with the new total of goals.
-# Tries to UPDATE all so need to work on the conditioning
-# Also, should this loop through table_rows or data?
-def update_top_scorer(connection, data, player_id, number_of_goals):
+# Updates the top_scorer with the new total of goals and inserts new scorers.
+def update_top_scorer(connection, data):
     try:
         c = connection.cursor()
+        player_names_for_rows_created = []
+        player_names_for_updates_made = []
 
-        table_rows = c.execute("SELECT * FROM top_scorer").rowcount
-        table_rows = c.fetchall()
+        # Loop for length of data retrieved because it will never be shorter than the length of table.
+        for i, value in enumerate(data):
+            data_i_row_player_id = data[i][0]
+            data_i_row_player_id_exists_check = "SELECT * FROM top_scorer WHERE player_id=" + str(data_i_row_player_id)
 
-        # Iterates through the table, using a SELECT command for each row
-        for i, value in enumerate(table_rows):
-            execute_command = "SELECT player_id, number_of_goals FROM top_scorer WHERE player_id=" + str(value[0]) + ";"
-            c.execute(execute_command)
-            row_player_id = [item for t in c.fetchall() for item in t]
+            data_i_row_select_all = select_from_table(connection, data_i_row_player_id_exists_check)
 
-            # If player_id in the table matches the player_id in the request
-            if row_player_id[0] == value[0] in player_id and row_player_id[0] == value[0] in data[i]:
-                # If the number of goals scored in the table matches the request
-                if row_player_id[1] == value[4] in number_of_goals and row_player_id[1] == value[4] in data[4]:
-                    print("Goals are the same")
-                else:
-                    print("Goal mismatch:\n  In table:", row_player_id[1], "\n  In request:", data[i][4])
+            # If select_from_table returns False then insert these players into the table
+            if not data_i_row_select_all:
+                insert_into_top_scorer(connection, data[i])
+                player_names_for_rows_created.append(data[i][1])
+            else:
+                data_i_row_goals_scored = data[i][4]
+
+                if data_i_row_goals_scored != data_i_row_select_all[4]:
                     execute_command = ("UPDATE top_scorer SET player_id=?, player_name=?, player_club_id=?,"
-                                        "player_club_name=?, number_of_goals=? WHERE player_id=" + str(row_player_id[0]))
+                                       "player_club_name=?, number_of_goals=?, player_position=? WHERE player_id="
+                                       + str(data_i_row_player_id))
                     execute_command = "".join(str(execute_command))
                     c.execute(execute_command, data[i])
                     connection.commit()
-                    print("Committed UPDATE to top_scorer")
+
+                    player_names_for_updates_made.append(data[i][1])
+
+        if player_names_for_rows_created:
+            print("Created", len(player_names_for_rows_created), "new rows for the following player/s:")
+            print(*player_names_for_rows_created, sep="\n")
+        if player_names_for_updates_made:
+            print("Updated", len(player_names_for_updates_made), "rows for the following player/s:")
+            print(*player_names_for_updates_made, sep="\n")
 
     except Error as e:
         print(e)
 
 
-# Insert the scorers to the top_scorer database. If data exists it will not
+# Insert the scorers to the top_scorer database. If data exists it will not.
 def insert_into_top_scorer(connection, data):
     try:
         c = connection.cursor()
-        c.executemany("""INSERT INTO top_scorer VALUES (?,?,?,?,?);""", data)
+        c.execute("""INSERT INTO top_scorer VALUES (?,?,?,?,?,?);""", data)
         connection.commit()
     except Error as e:
         print(e)
+
+
+# Creates a bar chart of the top five scorers from any data given.
+# Needs work
+def plot_top_scorers(data, graph_title):
+    players = []
+    goals = []
+    positions = []
+
+    # Populates the above lists with the data passed.
+    for i, value in enumerate(data):
+        players.append(data[i][0])
+        goals.append(int(data[i][1]))
+        positions.append(data[i][2])
+
+    # Shortens player's full names to just surnames.
+    for index, names in enumerate(players):
+        players[index] = names[names.index(" "):]
+
+    # For loop without iterator which saves me from having to find the length of players.
+    x_pos = [i for i, _ in enumerate(players)]
+
+    # Generates the colour of bars and the legend.
+    colors, legend = [], []
+    for index, position in enumerate(positions):
+        if positions[index] == "Attacker":
+            colors.append("r")
+            if "Red = Attacker" not in legend:
+                legend.append("Red = Attacker")
+        elif positions[index] == "Midfielder":
+            colors.append("b")
+            if "Blue = Midfielder" not in legend:
+                legend.append("Blue = Midfielder")
+        elif positions[index] == "Defender":
+            colors.append("g")
+            if "Green = Defender" not in legend:
+                legend.append("Green = Defender")
+
+    bars = plt.bar(x_pos, goals, align="center", alpha=0.3, color=colors)
+
+    axes = plt.gca()
+    axes.set_ylim([0, goals[0] + 6])
+
+    plt.ylabel("Goals Scored")
+    plt.xlabel("Player Name")
+    plt.title("Top Goalscorers (" + graph_title + ") in Premier League")
+    plt.legend(bars, legend)
+
+    plt.xticks(x_pos, players)
+
+    filename = "C:/Users/tomto/PycharmProjects/Football/" + get_usable_date() + graph_title
+    plt.savefig(filename, bbox_inches='tight')
+
+    plt.show()
 
 
 def main():
@@ -164,10 +234,10 @@ def main():
                                     player_name text,
                                     player_club_id integer,
                                     player_club_name text,
-                                    number_of_goals integer
+                                    number_of_goals integer,
+                                    player_position text
                                 );"""
-
-    print("From top_scorer:", select_from_table(database_connection, "SELECT * FROM top_scorer"))
+    sql_select_all = "SELECT * FROM top_scorer"
 
     # Opens connection to the database if one does not exist.
     if database_connection is not None:
@@ -175,59 +245,66 @@ def main():
     else:
         print("Error: Cannot create the database connection.")
 
-    # Can probably make this neater. Use lists and for loops?
-    # SELECT commands used to get dict of player_id and the corresponding number_of_goals
-    sql_goals_scored_player_id = "SELECT player_id FROM top_scorer;"
-    sql_goals_scored_player_name = "SELECT player_name FROM top_scorer;"
-    sql_goals_scored_number_of_goals = "SELECT number_of_goals FROM top_scorer;"
-
     # Call to API via function
     print("Sending request.")
     request_received = make_request(api_token)
-    print("From request:", request_received)
 
-    # If table is empty, fill with new call. If not empty then update any rows that need updating.
-    # Need to check if number of rows in table is less, more or equal to the request received.
-    if not select_from_table(database_connection, "SELECT * FROM top_scorer"):
+    # If table is empty, fill with new call. If not empty then update any rows that need updating
+    if not select_from_table(database_connection, sql_select_all):
         print("top_scorer is empty")
-        insert_into_top_scorer(database_connection, request_received)
+        insert_into_top_scorer(database_connection, request_received) # TEST THIS ACTUALLY WORKS
+        print("Table created with ", len(request_received), " rows.")
     else:
-        player_id = select_from_table(database_connection, sql_goals_scored_player_id)
-        number_of_goals = select_from_table(database_connection, sql_goals_scored_number_of_goals)
-
         # Commits the received request to the database
-        update_top_scorer(database_connection, request_received, player_id, number_of_goals)
+        update_top_scorer(database_connection, request_received)
 
-        # Closes connection as there's no need for it to be open anymore
-        close_connection(database_connection)
+    top_five_attackers = select_from_table(database_connection,
+                                           "SELECT player_name, number_of_goals, player_position FROM top_scorer "
+                                           "WHERE player_position='Attacker' ORDER BY number_of_goals DESC LIMIT 5")
+
+    top_five_midfielders = select_from_table(database_connection,
+                                             "SELECT player_name, number_of_goals, player_position FROM top_scorer "
+                                             "WHERE player_position='Midfielder' ORDER BY number_of_goals DESC LIMIT 5")
+
+    top_five_defenders = select_from_table(database_connection,
+                                           "SELECT player_name, number_of_goals, player_position FROM top_scorer "
+                                           "WHERE player_position='Defender' ORDER BY number_of_goals DESC LIMIT 5")
+
+    plot_top_scorers(np.array(top_five_attackers).reshape(-1, 3), "Attackers")
+    plot_top_scorers(np.array(top_five_midfielders).reshape(-1, 3), "Midfielders")
+    plot_top_scorers(np.array(top_five_defenders).reshape(-1, 3), "Defenders")
+    top_scoring_in_position = [top_five_attackers[0:3], top_five_midfielders[0:3], top_five_defenders[0:3]]
+    plot_top_scorers(top_scoring_in_position, "In Each Position")
+
+    close_connection(database_connection)
 
 
 if __name__ == "__main__":
     main()
 
+'''
+Get the plots working without static indices.
+
+What to do on plots when there are more than one player with the number of goals. Might need dict to do this because
+can't sort by number_of_goals AND alphabetical player_name.
+
+
+STATE OF PLAY:
+plotting works best when there's a nested list passed as the data so I don't need to specify the indices.
+need to find a way to convert the lists to nested lists.
+
+
+Start my own goalscorer tally:
+- add a table per game week so I can see averages etc.
+- call the API and work out number of goals scored per country
+
+Create a table for each player with over 3 goals to store when they were scored. Can make graphs from that.
+
+Could you connection.rollback() in excepts
 
 '''
-Start my own goalscorer tally:
-- create a call to the API that gets all matches played on current matchday and return playerIDs and numbers of goals scored.
-- will need to check if I need to write to the db in the first place.
-https://www.sqlitetutorial.net/sqlite-python/
-You shouldn't think of what you get as a "JSON object". What you have is a list. The list contains two dicts. The dicts
-contain various key/value pairs, all strings. When you do json_object[0], you're asking for the first dict in the list.
-When you iterate over that, with for song in json_object[0]:, you iterate over the keys of the dict. Because that's
-what you get when you iterate over the dict. If you want to access the value associated with the key in that dict, you
-would use, for example, json_object[0][song].
-'''
+
 '''
 GRAVEYARD:
-        # connection.request('GET', '/v2/competitions/PL/matches?timeFrame=n', None, headers)
-        # response = json.loads(connection.getresponse().read().decode())
-        # Ensures looking at the current season and matchday then looks for home team vs away team scores
-        # Integer doesn't behave as expected.
-        # Is this even needed? What's the point of it?
-        if current_season == response["matches"][10]["season"]["id"] and \
-                current_matchday == response["matches"][10]["season"]["currentMatchday"]:
-            print("Found current season and matchday:", current_season, "and", current_matchday)
-            print("Score:", response["matches"][21]["score"]["fullTime"])
-            print(response["matches"][21]["homeTeam"]["name"])
-            print(response["matches"][21]["awayTeam"]["name"])
+
 '''
